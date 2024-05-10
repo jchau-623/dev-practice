d036a4480fc29293078285ef89818f27eb8d537e
justin chau <jchau623@gmail.com>
2024-05-10 11:08:18 -0400

    navbar

--- react-app/src/NavBar/index.js
+++ react-app/src/NavBar/index.js
@@ -1,37 +1,51 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import './NavBar.css';
+import logo from '../assets/airbnb-logo.png';
 import LogoutButton from '../components/auth/LogoutButton'
 import LoginButton from '../components/auth/LoginButton';
-import { useDispatch, useSelector } from 'react-redux';
-import './NavBar.css'
-import logo from '../../assets/aibnb-logo.png';// Adjust the path accordingly
 
 const NavBar = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const user = useSelector(({ session }) => session.user);
 
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-logo">
           <NavLink to='/' exact={true} activeClassName='active'>
-            <div className='title-logo' alt='logo'>
-              <img className='logo' src={logo} alt='logo-pic'></img>
-            </div>
+            <img src={logo} alt='logo' />
           </NavLink>
-        </li>
-        {user ? (
-          // If user is logged in, show the logout button
-          <li>
+        </div>
+        <div className="navbar-search">
+          <input type="text" placeholder="Search destinations" className="nav-search-input"/>
+          <input type="text" placeholder="Check in" className="nav-search-input"/>
+          <input type="text" placeholder="Check out" className="nav-search-input"/>
+          <input type="number" placeholder="Add guests" min="1" className="nav-search-input"/>
+          <button className="search-btn">Search</button>
+        </div>
+        <div className="navbar-links">
+          <ul>
+            <li>
+              <NavLink to='/stays' activeClassName='active'>Stays</NavLink>
+            </li>
+            <li>
+              <NavLink to='/experiences' activeClassName='active'>Experiences</NavLink>
+            </li>
+            <li>
+              <NavLink to='/online-experiences' activeClassName='active'>Online Experiences</NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-user">
+          {user ? (
             <LogoutButton />
-          </li>
-        ) : (
-          // If user is not logged in, show the login button
-          <li>
+          ) : (
             <LoginButton />
-          </li>
-        )}
-      </ul>
+          )}
+        </div>
+      </div>
     </nav>
   );
 }
