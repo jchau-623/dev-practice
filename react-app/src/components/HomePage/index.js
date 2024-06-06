8016ddc917b32f0dbdad4a0f3c065821b6b62d26
justin chau <jchau623@gmail.com>
2024-06-06 14:56:54 -0400

    icon ribbon

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -3,18 +3,15 @@ import SpotList from '../SpotList';
 import IconRibbon from '../IconRibbon';
 
 export default function HomePage() {
-  // Use state to store spots data
   const [spots, setSpots] = useState([]);
 
   useEffect(() => {
-    // Fetch spots data from your API endpoint
     fetch('/api/spots')
       .then(response => response.json())
       .then(data => {
-        console.log(data); // Log the fetched data
-        setSpots(data.spots); // Update spots state with the array of spots
+        setSpots(data.spots);
       })
-      .catch(error => console.error('Error fetching spots:', error)); // Log any fetch errors
+      .catch(error => console.error('Error fetching spots:', error));
   }, []);
 
   return (
