68e5c05c7c3dd17c7688d973c2ba70911af3c4bf
justin chau <jchau623@gmail.com>
2024-06-07 13:50:28 -0400

    spot page

--- react-app/src/components/SpotPage/index.js
+++ react-app/src/components/SpotPage/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getSpots } from '../../store/spots';
 import { useParams } from 'react-router-dom';
+import './SpotPage.css';
 
 export default function SpotPage() {
     const dispatch = useDispatch();
@@ -18,16 +19,25 @@ export default function SpotPage() {
         return <div>Loading...</div>;
     }
 
+    const images = spot.images || [];
+
     return (
-        <div className="spot-card">
-            <div className="spot-details">
-                <div className="name">{spot.name}</div>
-                <div className="address">{spot.address}, {spot.city}, {spot.state}</div>
-                <div className="description">{spot.description}</div>
-                <div className="price">${spot.price} per night</div>
-                <div className="image">
-                    <img src={spot.image_url} alt={spot.name} />
-                </div>
+        <div className="spot-page">
+            <div className="spot-header">
+                <h1 className="name">{spot.name}</h1>
+                <p className="address">{spot.address}, {spot.city}, {spot.state}</p>
+                <img src={spot.image_url} alt={spot.name} />
+            </div>
+            <div className="spot-grid">
+                {images.map((image, index) => (
+                    <div key={index} className={`grid-item grid-item-${index}`}>
+                        <img src={image.url} alt={`Spot ${index + 1}`} />
+                    </div>
+                ))}
+            </div>
+            <div className="spot-info">
+                <p className="description">{spot.description}</p>
+                <p className="price">${spot.price} per night</p>
             </div>
         </div>
     );
